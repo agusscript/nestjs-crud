f6e6355a24532aeadced35aa434099b14755c644
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-12-26 13:23:31 -0300

    Add validations to customer dtos

--- src/customer/dto/update-customer.dto.ts
+++ src/customer/dto/update-customer.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
 export class UpdateCustomerDto {
   id: number;
+
+  @IsString()
+  @IsOptional()
   name?: string;
 }
